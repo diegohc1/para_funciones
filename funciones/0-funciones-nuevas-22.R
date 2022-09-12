8f50b058fa469fa746f888f2d31a19d96362be49
diegohc1 <dhermoza.c@gmail.com>
2022-09-12 10:48:38 -0500

    cambios pca

--- funciones/0-funciones-nuevas-22.R
+++ funciones/0-funciones-nuevas-22.R
@@ -238,7 +238,7 @@ pca_1 <- function(x){
   ee <- eigen(psych::polychoric(x)$rho, symmetric = FALSE)
   val <- ee$values #varianza
   l <- ee$vectors %*% diag(sqrt(val)) #cargas
-  if(all(l[1, ] < 0)) {l[1, ] <- l[1, ]*-1}
+  if(all(l[, 1] < 0)) {l[, 1] <- l[, 1]*-1} # ¿por que? U_U
   cargas <- data.frame(Item = names(x), Cargas = l[, 1])
   varex <- val[1]/sum(val)
   return(list(cargas = cargas, varex = varex))
